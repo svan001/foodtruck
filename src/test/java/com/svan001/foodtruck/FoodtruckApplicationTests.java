be0975ca0f6664d6baa026f804e7f612af8518d8
stephane <stephane.gronowski@gmail.com>
2018-10-28 10:32:59 +0100

    Initial commit after spring initialzr

---
+++ src/test/java/com/svan001/foodtruck/FoodtruckApplicationTests.java
@@ -0,0 +1,16 @@
+package com.svan001.foodtruck;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FoodtruckApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
