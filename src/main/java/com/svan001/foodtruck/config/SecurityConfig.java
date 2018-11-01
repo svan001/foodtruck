231a50cdacf80d52df8d970b84dea769c367aa1d
stephane <stephane.gronowski@gmail.com>
2018-11-01 22:25:08 +0100

    Fix broken spring security config (call to super). Add specific rule for static or public. Fun with burger and access denied page.

--- src/main/java/com/svan001/foodtruck/config/SecurityConfig.java
+++ src/main/java/com/svan001/foodtruck/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.svan001.foodtruck.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,19 +9,26 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Slf4j
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
         http
-                .authorizeRequests().antMatchers("/**")
-                .fullyAuthenticated()
+                .authorizeRequests().antMatchers("/static/img").permitAll()
+                .and().authorizeRequests().antMatchers("/public/**").permitAll()
+                // TODO to test access denied
+                .and().authorizeRequests().antMatchers("/alwaysdenied").denyAll()
                 // TODO Needed for H2 console => DEV TO REMOVE
-                .and().authorizeRequests().antMatchers("/console/**")
-                .permitAll();
+                .and().authorizeRequests().antMatchers("/console/**").permitAll()
+                .and().authorizeRequests().antMatchers("/test").fullyAuthenticated()
+                .and().authorizeRequests().antMatchers("/**").permitAll()
+                // basics
+                .and().formLogin()
+                .and().httpBasic()
+                .and().exceptionHandling().accessDeniedPage("/access_denied.html")
+        ;
 
         // TODO Needed for H2 console => DEV TO REMOVE
         http.csrf().disable();
